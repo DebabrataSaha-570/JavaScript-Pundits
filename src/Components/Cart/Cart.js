6aa32fadc49869060f5e787d4a7d2a4a1131d65d
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-04-01 14:04:33 +0600

    cart component basic sturcture done

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = (props) => {
     const { cart } = props;
+
+    let total = 0;
+    for (const programmer of cart) {
+        total = total + programmer.salary;
+
+    }
     return (
         <div>
-            <h3>Programmes Added: {cart.length}</h3>
+            <h4>Programmes Added: {cart.length}</h4>
+            <h5>Total cost: ${total}</h5>
+            {
+                cart.map(programmer => (
+                    <div className='d-flex align-items-center justify-content-around'>
+                        <img src={programmer.image} alt="" width="70" class="img-fluid mb-3 img-thumbnail shadow-sm" />
+                        <p>{programmer.name}</p>
+
+                        <FontAwesomeIcon className='btn btn-danger' icon={faXmark} size="lg" />
+
+                    </div>
+                ))
+            }
         </div>
     );
 };
